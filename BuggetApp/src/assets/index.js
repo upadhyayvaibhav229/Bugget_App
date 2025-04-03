e9fde31866b2ad0f541e8c2c6ee1a65856ab1dc3
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-04-04 01:28:35 +0530

    commited

---
+++ BuggetApp/src/assets/index.js
@@ -0,0 +1,9 @@
+import illstration from './Images/illustration.jpg'
+import logo from './Images/logo.svg'
+import account from './Images/account.svg'
+import wave from './Images/wave.png'
+export const img = {
+    illstration,
+    logo,
+    account
+}
